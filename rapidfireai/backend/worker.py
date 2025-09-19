bfd963df1dea7546cbba7505553f89ec64f3a78d
Pradyumna Sridhara <164606118+pradyumna-rfai@users.noreply.github.com>
2025-09-19 01:10:53 -0400

    Bugfix/chunking by batch size (#38)
    * Chunks: updated chunking algorithm to work with batch_size multiples
    
    * Tests: added test cases for chunks
    
    * Controller, Scheduler, Db: removed start_chunk_id
    
    * Controller, Chunk, Worker, Db: updated chunks to work with batch_size + offset for warm-start clones
    
    * effective batch size added
    
    ---------
    
    Co-authored-by: humaira-rf <humaira@rapidfire.ai>

--- rapidfireai/backend/worker.py
+++ rapidfireai/backend/worker.py
@@ -79,9 +79,8 @@ class Worker:
         DataPath.initialize(self.experiment_name, self.db.get_experiments_path(self.experiment_name))
 
         # load datasets
-        train_dataset, self.eval_dataset, self.num_chunks = self.load_datasets()
-        self.len_train_dataset = len(train_dataset)
-        self.train_dataset_chunks = DatasetChunks(train_dataset, self.num_chunks)
+        self.train_dataset, self.eval_dataset, self.num_chunks = self.load_datasets()
+        self.len_train_dataset = len(self.train_dataset)
 
     def load_datasets(
         self,
@@ -113,9 +112,16 @@ class Worker:
         # torch.manual_seed(run_details["seed"])
         # np.random.seed(run_details["seed"])
         # random.seed(run_details["seed"])
+        effective_batch_size = config_leaf["training_args"].get("per_device_train_batch_size", 1) * config_leaf["training_args"].get("gradient_accumulation_steps", 1)
 
         # fetch train dataset chunk
-        train_dataset_chunk = self.train_dataset_chunks.get_chunk(chunk_id)
+        train_dataset_chunker = DatasetChunks(
+            self.len_train_dataset,
+            self.num_chunks,
+            batch_size=effective_batch_size,
+            offset=run_details["chunk_offset"],
+        )
+        train_dataset_chunk = train_dataset_chunker.get_chunk(self.train_dataset, chunk_id)
         # create worker config
         trainer_config = TrainerConfig(
             worker_id=self.worker_id,
