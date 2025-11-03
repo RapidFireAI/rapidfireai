702729ba2877bf9c3e0678673c6e1c89420bc45c
David Matsalla <david@rapidfire.ai>
2025-11-03 03:04:11 -0800

    colabcu126

--- rapidfireai/cli.py
+++ rapidfireai/cli.py
@@ -336,7 +336,7 @@ def install_packages(evals: bool = False):
         packages.append({"package": "torchvision==0.20.1", "extra_args": ["--upgrade", "--index-url", "https://download.pytorch.org/whl/cu126"]})
         packages.append({"package": "torchaudio==2.5.1", "extra_args": ["--upgrade", "--index-url", "https://download.pytorch.org/whl/cu126"]})
         packages.append({"package": "vllm==0.7.2", "extra_args": ["--torch-backend=cu124"]})
-        packages.append({"package": "flashinfer-python==0.2.5", "extra_args": ["--index-url", "https://flashinfer.ai/whl/cu126/torch2.5/"]})
+        packages.append({"package": "flashinfer-python==0.2.5", "extra_args": ["--index-url", "https://flashinfer.ai/whl/cu124/torch2.5/"]})
     # elif cuda_major == 11:
     #     print(f"\n🎯 Detected CUDA {cuda_major}.x")
     #     packages.append({"package": "vllm==0.10.1.1", "extra_args": ["--torch-backend=cu118"]})
