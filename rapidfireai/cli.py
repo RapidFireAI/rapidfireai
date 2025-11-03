bdb5d13d28aae6a9735376f5775e3b4cea00f26c
David Matsalla <david@rapidfire.ai>
2025-11-02 21:05:19 -0800

    pyproject

--- rapidfireai/cli.py
+++ rapidfireai/cli.py
@@ -333,7 +333,7 @@ def install_packages(evals: bool = False):
     if evals and cuda_major == 12:
         print(f"\n🎯 Detected CUDA {cuda_major}.x")
         packages.append({"package": "vllm==0.7.2", "extra_args": ["--torch-backend=cu124"]})
-        packages.append({"package": "flashinfer-python==0.2.5", "extra_args": ["--index-url https://flashinfer.ai/whl/cu124/torch2.5/"]})
+        packages.append({"package": "flashinfer-python==0.2.5", "extra_args": ["--index-url", "https://flashinfer.ai/whl/cu124/torch2.5/"]})
     # elif cuda_major == 11:
     #     print(f"\n🎯 Detected CUDA {cuda_major}.x")
     #     packages.append({"package": "vllm==0.10.1.1", "extra_args": ["--torch-backend=cu118"]})
