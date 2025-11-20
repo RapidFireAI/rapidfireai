928ea3d9dfa3aca52b5caf1e64401731b4cee449
David Matsalla <david@rapidfire.ai>
2025-11-19 21:41:26 -0800

    Fix vllm

--- rapidfireai/cli.py
+++ rapidfireai/cli.py
@@ -489,7 +489,7 @@ def install_packages(evals: bool = False):
         packages.append({"package": f"torchaudio=={torchaudio_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
         # Install vllm before torch to avoid version conflicts
         # packages.append({"package": f"vllm=={vllm_version}", "extra_args": ["--upgrade", "--extra-index-url", f"https://download.pytorch.org/whl/{vllm_cuda}"]})
-        packages.append({"packages": f"vllm=={vllm_version}", "extra_args": ["--upgrade"]})
+        packages.append({"package": f"vllm=={vllm_version}", "extra_args": ["--upgrade"]})
         # packages.append({"package": "faiss-gpu-cu12==1.12.0", "extra_args": []})
         # packages.append({"package": "flashinfer-python==0.2.5", "extra_args": ["--index-url", "https://flashinfer.ai/whl/cu124/torch2.5/"]})
         # packages.append({"package": "flash-attn", "extra_args": ["--no-build-isolation"]})
