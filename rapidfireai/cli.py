6575a76af088d7ac2bbd56f6b7fc6e1e811ac675
David Matsalla <david@rapidfire.ai>
2025-12-05 00:26:19 -0800

    Add optimized torch to fit

--- rapidfireai/cli.py
+++ rapidfireai/cli.py
@@ -203,24 +203,24 @@ def install_packages(evals: bool = False):
 
     
     ## TODO: re-enable for fit once trl has fix
-    if evals and not ColabConfig.ON_COLAB and cuda_major >= 12:
-        
+    if not ColabConfig.ON_COLAB and cuda_major >= 12:
         print(f"\n🎯 Detected CUDA {cuda_major}.{cuda_minor}, using {torch_cuda}")
         
         packages.append({"package": f"torch=={torch_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
         packages.append({"package": f"torchvision=={torchvision_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
         packages.append({"package": f"torchaudio=={torchaudio_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
-        packages.append({"package": f"vllm=={vllm_version}", "extra_args": ["--upgrade"]})
-        packages.append({"package": "flashinfer-python", "extra_args": []})
-        packages.append({"package": "flashinfer-cubin", "extra_args": []})
-        if cuda_major + (cuda_minor / 10.0) >= 12.8:
-            packages.append({"package": "flashinfer-jit-cache", "extra_args": ["--upgrade","--index-url", f"https://flashinfer.ai/whl/{flash_cuda}"]})
-        packages.append({"package": "flash-attn==2.8.3", "extra_args": ["--upgrade", "--no-build-isolation"]})
-        # packages.append({"package": "https://github.com/RapidFireAI/faiss-wheels/releases/download/v1.13.0/rf_faiss_gpu_12_8-1.13.0-cp39-abi3-manylinux_2_34_x86_64.whl", "extra_args": []})
-        # Re-install torch, torchvision, and torchaudio to ensure compatibility
-        packages.append({"package": f"torch=={torch_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
-        packages.append({"package": f"torchvision=={torchvision_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
-        packages.append({"package": f"torchaudio=={torchaudio_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
+        if evals:
+            packages.append({"package": f"vllm=={vllm_version}", "extra_args": ["--upgrade"]})
+            packages.append({"package": "flashinfer-python", "extra_args": []})
+            packages.append({"package": "flashinfer-cubin", "extra_args": []})
+            if cuda_major + (cuda_minor / 10.0) >= 12.8:
+                packages.append({"package": "flashinfer-jit-cache", "extra_args": ["--upgrade","--index-url", f"https://flashinfer.ai/whl/{flash_cuda}"]})
+            packages.append({"package": "flash-attn==2.8.3", "extra_args": ["--upgrade", "--no-build-isolation"]})
+            # packages.append({"package": "https://github.com/RapidFireAI/faiss-wheels/releases/download/v1.13.0/rf_faiss_gpu_12_8-1.13.0-cp39-abi3-manylinux_2_34_x86_64.whl", "extra_args": []})
+            # Re-install torch, torchvision, and torchaudio to ensure compatibility
+            packages.append({"package": f"torch=={torch_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
+            packages.append({"package": f"torchvision=={torchvision_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
+            packages.append({"package": f"torchaudio=={torchaudio_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
 
     # TODO: re-enable for fit once flash-attn has fix
 
