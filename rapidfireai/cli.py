fb7b2234d47708beffada8c8eeaea96aaedf67af
David Matsalla <david@rapidfire.ai>
2025-12-09 01:08:29 -0800

    Add triton flash

--- rapidfireai/cli.py
+++ rapidfireai/cli.py
@@ -218,6 +218,8 @@ def install_packages(evals: bool = False, init_packages: list[str] | None = None
                 packages.append({"package": "flashinfer-jit-cache", "extra_args": ["--upgrade","--index-url", f"https://flashinfer.ai/whl/{flash_cuda}"]})
             if get_compute_capability() >= 8.0:
                 packages.append({"package": "flash-attn>=2.8.3", "extra_args": ["--upgrade", "--no-build-isolation"]})
+            else:
+                packages.append({"package": "flash-attn-triton", "extra_args": ["--upgrade"]})
             # packages.append({"package": "https://github.com/RapidFireAI/faiss-wheels/releases/download/v1.13.0/rf_faiss_gpu_12_8-1.13.0-cp39-abi3-manylinux_2_34_x86_64.whl", "extra_args": []})
             # Re-install torch, torchvision, and torchaudio to ensure compatibility
             packages.append({"package": f"torch=={torch_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
