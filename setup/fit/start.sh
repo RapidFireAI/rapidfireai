3ee92d59e6f2855ac372e2c7838810fae399909b
David Matsalla <david@rapidfire.ai>
2025-12-23 22:04:37 -0800

    Remove pins from dependencies

--- setup/fit/start.sh
+++ setup/fit/start.sh
@@ -119,7 +119,7 @@ setup_python_env() {
 # Function to cleanup processes on exit
 cleanup() {
     set +x
-    if [[ "$RF_FORCE" != "true" ]]; then
+    if [ "$RF_FORCE" != "true" ]; then
         # Confirm cleanup
         read -p "Do you want to shutdown services and delete the PID file? (y/n) " -n 1 -r REPLY
         echo
