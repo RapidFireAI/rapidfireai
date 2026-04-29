23c0cafb9f997c3f235e4f01eca7028cd1bcf8ef
David Matsalla <david@rapidfire.ai>
2026-04-28 19:12:58 -0700

    vllm auto

--- rapidfireai/cli.py
+++ rapidfireai/cli.py
@@ -333,7 +333,7 @@ def install_packages(
         packages.append({"package": f"torchvision=={torchvision_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
         packages.append({"package": f"torchaudio=={torchaudio_version}", "extra_args": ["--upgrade", "--index-url", f"https://download.pytorch.org/whl/{torch_cuda}"]})
         if evals:
-            packages.append({"package": f"vllm=={vllm_version}", "extra_args": ["--upgrade"]})
+            packages.append({"package": f"vllm=={vllm_version}", "extra_args": ["--torch-backend=auto","--upgrade"]})
             packages.append({"package": "flashinfer-python", "extra_args": ["--upgrade"]})
             packages.append({"package": "flashinfer-cubin", "extra_args": ["--upgrade"]})
             if cuda_major + (cuda_minor / 10.0) >= 12.8:
